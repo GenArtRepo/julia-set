d9e89cbbf24fec2330f826b010085343aad848e5
Cristian Rojas <cristian.rojascardenas@monash.edu>
2022-05-03 13:55:00 +1000

    Animation fluidly working now

--- sketch.js
+++ sketch.js
@@ -1,7 +1,123 @@
+/*
+** Julia Set
+* Cristian Rojas Cardenas, April 2022
+* Algorithm based on the tutorial of Daniel Shiffman.
+* See the video here: 
+* https://www.youtube.com/watch?v=k6OeXGzRv0c
+
+
+http://paulbourke.net/fractals/juliaset/
+
+*/
+
+
+let c1;
+let c2;
+
+
+let settings = { 
+    Generate: function(){ init(); },
+    max_iterations: 100,
+    infinity_value: 16,
+    gray_scale: false,
+    
+}
+
+function gui(){
+    // Adding the GUI menu
+    var gui = new dat.GUI();
+    gui.width = 150;
+    gui.add(settings,'Generate');
+    gui.add(settings,'max_iterations', 10, 200).step(1);
+    gui.add(settings,'infinity_value', 1, 100).step(1);
+    gui.add(settings,'gray_scale');
+}
+
+
 function setup(){
-
+    gui();
+    createCanvas(720, 400);
+    pixelDensity(1);
+    init();
 }
 
-function draw(){
 
+
+function init(){
+    background(0);
+
+    c1 = color(0, 0, 139);
+    c2 = color(255, 255, 255);
+    
+    
+    loadPixels();
+    for (let i = 0; i < width; i++) {
+        for (let j = 0; j < height; j++) { 
+            n =  maldelbrot(i, j);
+            n_color = getColor(n);
+            setPixels(i, j, n_color.levels);
+        }
+    }
+    updatePixels();
 }
+
+function draw(){}
+
+function maldelbrot(i, j){
+
+    // The grid reference is translated and scaled
+    var a = map(i, 0, width, -2.5, 1.5);
+    var b = map(j, 0, height, -1.5, 1.5);
+
+    // C takes the value of the current point
+    var ca = a;
+    var cb = b;
+
+    // Iterations counter
+    var n = 0;
+
+    while (n < settings.max_iterations){
+
+        // Z^2
+        var real = a*a - b*b;
+        var imag = 2*a*b;
+
+        // Zn+1 = Zn + c
+        a = real - 0.70176;
+        b = imag - 0.3842;
+
+        // The process continues until it reachs the infinity value set as 16.
+        // The area that points belong depends on how fast it reachs the infinity
+        // value 
+        if (dist(a*a, b*b, 0, 0) > settings.infinity_value){
+            break;
+        }
+        n++;
+    }
+
+    return n
+}
+
+function getColor(n){
+
+    var factor = map(n, 0, settings.max_iterations, 0, 1);
+    if(settings.gray_scale){
+        bright = map(sqrt(factor), 0, 1, 0, 255);
+        n_color = color(bright, bright, bright);
+    } 
+    else{
+        if(n==settings.max_iterations) n_color = color(0, 0, 0);
+        else n_color = lerpColor(c1, c2, factor);
+    }
+
+    return n_color;
+}
+
+function setPixels(i, j, color){
+    var pixel = 4*(i + j*width);
+    pixels[pixel] = color[0];
+    pixels[pixel+1] = color[1];
+    pixels[pixel+2] = color[2];
+    pixels[pixel+3] = 255;
+}
+
